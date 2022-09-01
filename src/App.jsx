877a3d7c213d9908aba40d452b94be8317f1d656
Ahindra D <official.ahindra@gmail.com>
2022-09-01 14:11:59 +0530

    fetching id

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { socket, SocketContext } from './contexts/SocketContext';
 
 
 export default function App() {
+    console.log(socket);
     return (
         <SocketContext.Provider value={socket}>
             <div>App</div>
