3b422c4b1f76c991a5aba96890313b4ac1007673
Ahindra D <official.ahindra@gmail.com>
2022-09-04 22:15:49 +0530

    env not importing

--- src/contexts/SocketContext.jsx
+++ src/contexts/SocketContext.jsx
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 import io from "socket.io-client";
 
-const socket = io(process.env.REACT_APP_SOCKET_URL, { transports: ['websocket'] });
+const socket = io("https://tic-tac-toe-servr.herokuapp.com/", { transports: ['websocket'] });
 const SocketContext = createContext(null);
 
 export { socket, SocketContext };
